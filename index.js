92e1ba57c8c612c5c5ff870ff6867adcd496b31d
Rhys <rhys@rhysjenkins.uk>
2023-11-15 21:54:41 +0000

    Added script

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	res.sendFile('views/test.html', { root: __dirname });
+app.get("/", (req, res) => {
+    res.sendFile("views/test.html", { root: __dirname });
 });
 
-app.post('/test', (req, res) => {
-	console.log(req.body);
-	res.redirect('/')
-})
+app.post("/test", (req, res) => {
+    console.log(req.body);
+    res.redirect("/");
+});
 
-app.get('/download', (req, res) => {
-	res.download('rhys-jenkins-cv.pdf');
+app.get("/download", (req, res) => {
+    res.download("rhys-jenkins-cv.pdf");
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
 
 // *** To do ***
-
